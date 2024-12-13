f3c3c8d5da3f22b5476cb8357ab1c2e6d4871b60
flavi13 <103426392+flavi13@users.noreply.github.com>
2024-12-13 10:51:13 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Flavia Rivero on 4/12/24.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by Flavia Rivero on 4/12/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
